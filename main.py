8ee0cda0feae088161e68be99b6a124d72648594
realkosmoss <113364969+realkosmoss@users.noreply.github.com>
2025-03-14 14:46:40 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,83 @@
+import discord
+from discord.ext import commands
+import time
+import json
+
+import custom.AI as AI
+from custom.commands import Commands as Bot_Commands
+from custom.messages import Messages
+from custom.lore_handling import LoreHandling
+
+def Get_Messages():
+    try:
+        with open(MESSAGES_PATH, 'r') as file:
+            return json.load(file)
+    except (json.JSONDecodeError, FileNotFoundError):
+        return []
+
+MESSAGES_PATH = "messages.json"
+IN_USE = False
+
+TOKEN = "pee pee poo poo!"
+
+messages_instance = Messages()
+bot_commands = Bot_Commands(messages_instance)
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
+
+@bot.event
+async def on_message(message: discord.Message):
+    global IN_USE
+    if message.author == bot.user:
+        return
+    
+    if not message.channel.category_id == 1349346892722737265 or message.channel.category is None:  # public server use shit
+        lorehandler = LoreHandling(message.author.id, message.channel.id)
+        _abc = False
+        for i in lorehandler.channels:
+            if i == message.channel.id:
+                messages_instance_temp = lorehandler
+                _abc = True
+                break
+        if not _abc:
+            return
+    else:
+        messages_instance_temp = messages_instance
+
+    if message.content.startswith("!"):
+        await bot_commands.ProcessComands(message)
+        return
+    if IN_USE:
+        while IN_USE:
+            time.sleep(1)
+            if not IN_USE:
+                IN_USE = True
+                break
+    async with message.channel.typing():
+        start = time.time()
+        response = AI.Get_Response(message, messages_instance_temp.messages)
+        total = time.time() - start
+        if message is None or not message:
+            return
+        USERNAME = message.author.display_name if message.author.display_name else message.author.global_name
+        CONTENT = message.content
+        messages_instance_temp.messages.append({
+            "role": "user",
+            "content": f"{USERNAME}: {CONTENT}"
+        })
+        messages_instance_temp.messages.append({
+            "role": "assistant",
+            "content": response
+        })
+        messages_instance_temp.save()
+        await message.reply(f"{response}\n-# Took: {total:.2f} <3")
+    await bot.process_commands(message)
+
+
+bot.run(TOKEN)
---
+++ start.bat
@@ -0,0 +1,18 @@
+@echo off
+set VENV_DIR=venv
+
+echo Making venv and installing requirements.txt
+if not exist %VENV_DIR% (
+    echo Creating virtual environment...
+    python -m venv %VENV_DIR%
+)
+
+call %VENV_DIR%\Scripts\activate
+
+pip install -r requirements.txt
+
+echo Running main.py
+python main.py
+
+
+cmd /k
